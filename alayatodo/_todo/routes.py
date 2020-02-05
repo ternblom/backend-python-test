1a36a82fdd5cfe93e0004350e538531610a7148e
Arvid Ternblom <arvidternblom@gmail.com>
2020-02-05 22:29:55 +0800

    Task 5: Added todo filter by user logged in

--- alayatodo/_todo/routes.py
+++ alayatodo/_todo/routes.py
@@ -1,30 +1,28 @@
-from flask import g, render_template, request, session, redirect, flash
+from flask import render_template, request, session, redirect, flash, abort, url_for
 from flask_login import current_user, login_required
 from alayatodo import db
 from alayatodo.models import User, Todo, TodoSchema
 from alayatodo._todo import bp
-from functools import wraps
 
 
 @bp.route('/todo/<id>', methods=['GET'])
 @login_required
 def todo(id):
-    todo = Todo.query.get_or_404(id)
-    return render_template('todo.html', todo=todo)
+    todo = Todo.query.filter_by(user=current_user, id=id).first()
+    if todo:
+        return render_template('todo.html', todo=todo)
+    abort(404)
 
 
 @bp.route('/todo', methods=['GET'])
-@bp.route('/todo/', methods=['GET'])
 @login_required
 def todos():
-    if not session.get('logged_in'):
-        return redirect('/login')
-    todos = Todo.query.all()
+    page = request.args.get('page', 1, type=int)
+    todos = Todo.query.filter_by(user=current_user).order_by(Todo.id.desc()).paginate(page=page, per_page=5)
     return render_template('todos.html', todos=todos)
 
 
 @bp.route('/todo', methods=['POST'])
-@bp.route('/todo/', methods=['POST'])
 @login_required
 def todos_POST():
     try:
@@ -35,36 +33,38 @@ def todos_POST():
     except AssertionError as err:
         flash(err.args[0], 'danger')
         db.session.rollback()
-    return redirect('/todo')
+    return redirect(url_for('_todo.todos'))
 
 
 @bp.route('/todo/<id>', methods=['DELETE'])
 def todo_delete(id):
     if not current_user.is_authenticated:
         return ('To perform this action you must be logged in!', 401)
-    todo = Todo.query.get(id)
+    todo = Todo.query.filter_by(user=current_user, id=id).first()
     if todo:
         db.session.delete(todo)
         db.session.commit()
         return 'ToDo deleted!'
     return ('Oops, ToDo not found!', 404)
 
+
 @bp.route('/todo/<id>/complete', methods=['PUT'])
 def todo_complete(id):
     if not current_user.is_authenticated:
         return ('To perform this action you must be logged in!', 401)
-    todo = Todo.query.get(id)
+    todo = Todo.query.filter_by(user=current_user, id=id).first()
     if todo:
         todo.completed = True
         db.session.commit()
         return 'ToDo Ready!'
     return ('Oops, ToDo not found!', 404)
 
+
 @bp.route('/todo/<id>/json', methods=['GET'])
 def todo_json(id):
     if not current_user.is_authenticated:
         return ('To perform this action you must be logged in!', 401)
-    todo = Todo.query.get(id)
+    todo = Todo.query.filter_by(user=current_user, id=id).first()
     if todo:
         todo_schema = TodoSchema()
         return todo_schema.dump(todo)
